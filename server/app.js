13d8d8cadf3447ab8aecf1f13d69ed48d76f5cb6
Andrej Rakic <andrejrakic@Andrejs-MacBook-Air.local>
2020-01-07 21:32:02 +0100

    Mongoose connected with Atlas

--- server/app.js
+++ server/app.js
@@ -2,12 +2,21 @@ const express = require('express');
 const app = express();
 const port = 4000;
 const io = require('socket.io').listen(4001).sockets;
+const mongoose = require('mongoose');
 
 app.get('/', (req, res) => res.send('Hello from Chat app'));
 
 app.listen(port, () =>
 	console.log(`🚀  Chat app is listening on port ${port}!`)
 );
+
+mongoose
+	.connect(
+		`mongodb+srv://andrej:naprednebazepodataka@cluster0-bqac9.mongodb.net/test`,
+		{ useNewUrlParser: true, useUnifiedTopology: true }
+	)
+	.then(() => console.log(`🖐️  Mongoose connected to DB`))
+	.catch(err => console.log(err));
 
 io.on('connection', function(socket) {
 	//socket.emit('news', { hello: 'world' });
